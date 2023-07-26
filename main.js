30f8acf099e27cf7c6329341cbd638bf9022e0a5
Aubrey <116300259+Br3y@users.noreply.github.com>
2023-07-27 03:41:09 +0800

    changing fetch to async wait

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#currency-converter").addEventListener("submit", (event) => {
+    // document.querySelector("#currency-converter").addEventListener("submit", (event) => {
+    document.querySelector("#currency-converter").addEventListener("submit", async (event) => {
         event.preventDefault();
 
         const { target: { to, from, amount } } = event;
@@ -11,20 +12,37 @@ document.addEventListener("DOMContentLoaded", () => {
             redirect: 'follow',
             headers: myHeaders
         };
+        // old way
 
-        fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
-            .then(response => response.json())
-            .then(data => {
-                let { info, date, query:{from, to}, result } = data;
-                document.querySelector(".result").textContent = 
+        // fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         let { info, date, query:{from, to}, result } = data;
+        //         document.querySelector(".result").textContent = 
+        //         `rate: ${info.rate.toFixed(2)}
+        //          date: ${date}
+        //          from: ${from}
+        //          to: ${to}
+        //          result: ${result.toFixed(2)}`;
+
+        // new way of api with try cath 
+        try{
+            let response = await fetch(`https://api.apilayer.com/exchangerates_data/convert?to=${to.value}&from=${from.value}&amount=${amount.valueAsNumber}`, requestOptions)
+            const data = await response.json();
+            console.log(data);
+            let { info, date, query, result } = data
+            document.querySelector(".result").textContent =
                 `rate: ${info.rate.toFixed(2)}
-                 date: ${date}
-                 from: ${from}
-                 to: ${to}
-                 result: ${result.toFixed(2)}`;
-
-                //  console.log(data);
-            })
-            .catch(error => console.log('error', error));
+                date: ${date}
+                from: ${query.from}
+                to: ${query.to}
+                result: ${result.toFixed(2)}`;
+        }
+        catch(error){
+            console.log(error);
+        }
+        finally{
+            console.log("finally block");
+        }
     })
-})
+})  
