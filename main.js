f7c0bf28787072ed68d4a2ad6eba85fac4abaea2
Aubrey <116300259+Br3y@users.noreply.github.com>
2023-08-02 03:53:05 +0800

    finishing weather app w/o search, comeback after getting better

--- main.js
+++ main.js
@@ -2,9 +2,15 @@
 const API_KEY = "26ebec346666c60ecc110306d8460dba";
 const formatTemperature = (temp) => `${temp?.toFixed(1)}°`
 const createIconUrl = (icon) => `https://openweathermap.org/img/wn/${icon}@2x.png`;
-const DAYS_OF_THE_WEEK = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-
+const DAYS_OF_THE_WEEK = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
+let selectedCityText;
+let selectedCity;
+     
 // GET API FROM WEATHERMAP
+const getCitiesUsingGeolocation = async (searchText) =>{
+    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit={limit}5&appid=${API_KEY}`)
+    return response.json();
+}
 const getCurrentWeatherData = async () => {
     const city = "calamba";
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
@@ -108,8 +114,53 @@ const loadHumidity = ({ main: { humidity } }) => {
     container.querySelector(".humidity-value").textContent = `${humidity} %`;
 }
 
+function debounce(func){
+    let timer;
+    return (...args) =>{
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            console.log("debounce");
+            func.apply(this, args)
+        }, 500);
+    }
+}
+const onSearchChange = async (event) =>{
+    let {value} = event.target;
+    if(!value){
+        selectedCity = null;
+        selectedCityText = "";
+    }
+    if(value && selectedCity !== value){
+        const listOfCities = await getCitiesUsingGeolocation(value)
+        let options = "";
+        for(let {lat, lon, name, state, country} of listOfCities){
+            options += `<option data-city-details="${JSON.stringify({lat, lon, name})}" value="${name}, ${state}, ${country}"></option>` 
+        }
+        document.querySelector("#cities").innerHTML = options;
+        console.log(listOfCities);
+    }
+}
+const handleCitySelection = (event) => {
+    console.log("selection done");
+    selectedCityText = event.target.value;
+    let options = document.querySelectorAll("#cities > option");
+    console.log(options);
+    if(options?.length){
+        let selectedOption = Array.from(options).find(opt => opt.value === selectedCityText);
+        selectedCity = JSON.parse(selectedOption.getAttribute("data-city-details"));
+        console.log({selectedCity});
+    }
+}
+
+const debounceSearch = debounce((event) => onSearchChange(event));
+
 // CALLING function
 document.addEventListener("DOMContentLoaded", async () => {
+
+    const searchInput = document.querySelector("#search");
+    searchInput.addEventListener("input", debounceSearch);
+    searchInput.addEventListener("change", handleCitySelection)
+
     const currentWeather = await getCurrentWeatherData();
     loadCurrentForecast(currentWeather);
     const hourlyForecast = await getHourlyForecast(currentWeather);
